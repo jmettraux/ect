b4ac2f52d788bebb2c56cbb2685380004e8d5c4f
John Mettraux <jmettraux@gmail.com>
2017-10-12 16:26:14 +0900

    Reorganize Enumerable spec

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -8,42 +8,44 @@
 require 'spec_helper'
 
 
-describe 'Enumerable#bisect' do
+describe Enumerable do
 
-  it 'is an alias to partition' do
+  describe '#bisect' do
 
-    expect(
-      (1..21).bisect(&:odd?)
-    ).to eq(
-      (1..21).partition(&:odd?)
-    )
-  end
-end
+    it 'is an alias to partition' do
 
-
-describe 'Enumerable#dissect' do
-
-  it 'partitions into multiple arrays' do
-
-    a0, a1, a2 = (1..14).dissect { |i| i % 3 }
-
-    expect(a0).to eq([ 3, 6, 9, 12 ])
-    expect(a1).to eq([ 1, 4, 7, 10, 13 ])
-    expect(a2).to eq([ 2, 5, 8, 11, 14 ])
+      expect(
+        (1..21).bisect(&:odd?)
+      ).to eq(
+        (1..21).partition(&:odd?)
+      )
+    end
   end
 
-  it 'returns nil instead of unpopulated arrays' do
+  describe '#dissect' do
 
-    a0, a1, a2 = (1..14)
-      .dissect { |i|
-        case m = i % 3
-        when 0, 2 then m
-        else 0
-        end }
+    it 'partitions into multiple arrays' do
 
-    expect(a0).to eq([ 1, 3, 4, 6, 7, 9, 10, 12, 13 ])
-    expect(a1).to eq(nil)
-    expect(a2).to eq([ 2, 5, 8, 11, 14 ])
+      a0, a1, a2 = (1..14).dissect { |i| i % 3 }
+
+      expect(a0).to eq([ 3, 6, 9, 12 ])
+      expect(a1).to eq([ 1, 4, 7, 10, 13 ])
+      expect(a2).to eq([ 2, 5, 8, 11, 14 ])
+    end
+
+    it 'returns nil instead of unpopulated arrays' do
+
+      a0, a1, a2 = (1..14)
+        .dissect { |i|
+          case m = i % 3
+          when 0, 2 then m
+          else 0
+          end }
+
+      expect(a0).to eq([ 1, 3, 4, 6, 7, 9, 10, 12, 13 ])
+      expect(a1).to eq(nil)
+      expect(a2).to eq([ 2, 5, 8, 11, 14 ])
+    end
   end
 end
 
